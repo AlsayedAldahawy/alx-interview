635902e06e8d506455d47e75b7ba62e0eb017315
Alsayedaldahawy <alsayedaldahawy@outlook.com>
2024-08-11 05:01:19 +0300

    INTERVIEW0-PASCALTRI-Done

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,3 +1,16 @@
 #!/usr/bin/python3
 
+def pascal_triangle(n):
 
+    tri = []
+
+    for i in range(1, n + 1):
+        row = [1] * i
+        for j in range(i):
+            if j == 0 or j == i - 1:
+                continue
+            else:
+                row[j] = tri[-1][j] + tri[-1][j - 1]
+
+        tri.append(row)
+    return (tri)
