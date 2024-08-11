aaeba3f3132a2c4a2ad7bfa0ca48baff8e5f62b4
Alsayedaldahawy <alsayedaldahawy@outlook.com>
2024-08-11 05:04:20 +0300

    INTERVIEW0-PASCALTRI-Documentation

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,7 +1,22 @@
 #!/usr/bin/python3
 
+'''
+    Module contains Pascal's Triangle gemerator function.
+'''
 def pascal_triangle(n):
+    """
+    Generates Pascal's triangle up to the specified number of rows.
 
+    Args:
+        n (int): Number of rows in the Pascal's triangle.
+
+    Returns:
+        list of lists: A list of lists representing Pascal's triangle.
+
+    Example:
+        >>> pascal_triangle(5)
+        [[1], [1, 1], [1, 2, 1], [1, 3, 3, 1], [1, 4, 6, 4, 1]]
+    """
     tri = []
 
     for i in range(1, n + 1):
@@ -13,4 +28,5 @@ def pascal_triangle(n):
                 row[j] = tri[-1][j] + tri[-1][j - 1]
 
         tri.append(row)
-    return (tri)
+    return tri
+
