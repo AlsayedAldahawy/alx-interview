2cfd798ea8c2feb5e807a207b5ce42059a17e794
Alsayedaldahawy <alsayedaldahawy@outlook.com>
2024-09-12 19:07:22 +0300

    ALX-INTRVIEW-02

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -8,15 +8,16 @@ def minOperations(n):
     if n <= 1:
         return 0
 
-    minOps = 0
-    i = 0
-    while i * i <= n:
+    operations = 0
+    divisor = 2
 
-        for i in range(2, int(n ** 0.5) + 1):
-            if n % i == 0:
-                n //= i
-                minOps += i
-        if i == int(n ** 0.5):
-            break
-    minOps += n
-    return minOps
+    while divisor * divisor <= n:
+        while n % divisor == 0:
+            operations += divisor
+            n //= divisor
+        divisor += 1
+
+    if n > 1:
+        operations += n
+
+    return operations
